702aa90c51d8de5e71efc602f12cf8c78d27f5fb
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2023-11-11 11:57:49 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+document.getElementById("goButton").addEventListener("click", function () {
+    var selectedMechanic = document.getElementById("mechanicDropdown").value;
+    var selectedStation = document.getElementById("stationDropdown").value;
+
+    selectedMechanic = selectedMechanic.toUpperCase().replace(" ", "_");
+    selectedStation = selectedStation.toUpperCase().replace(" ", "_");
+
+    var redirectionURLs = {
+        "A": baseRedirectURL + "&prefill_Station=" + selectedStation,
+        "D": baseRedirectURL + "&prefill_Station=" + selectedStation,
+    };
+
+    if (redirectionURLs[selectedStation]) {
+        window.location.href = redirectionURLs[selectedStation];
+    } else {
+        alert("Wybierz linie.");
+    }
+});
